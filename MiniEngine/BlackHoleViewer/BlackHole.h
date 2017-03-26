d9f301827dddffa7f1d42a78869c921420961a8f
Snowball <snowball@live.ru>
2017-03-26 12:00:56 +0300

    black hole model class created

---
+++ MiniEngine/BlackHoleViewer/BlackHole.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/* Black hole physical model (mass, lighspeed, accreation disk size, accreation disk color, texture and so on) */
+class BlackHole
+{
+private:
+	double m_mass;
+	double m_lightspeed;
+};
